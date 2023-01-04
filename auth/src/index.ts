791c20b497f25eb443717a1b4c9f067178c0d7cc
Braian Gonzales <braian.gonzales@wakapi.com>
2023-01-04 16:50:53 -0300

    feat(auth): add signout logic

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signt";
+import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import cookieSession from "cookie-session";
