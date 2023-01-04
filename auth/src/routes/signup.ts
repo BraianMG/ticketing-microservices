7346d988a4cfe72e5932d1f3e9ac0be104619561
Braian Gonzales <braian.gonzales@wakapi.com>
2023-01-04 14:49:33 -0300

    feat(auth): generate a JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express, {Request, Response} from "express";
 import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import jwt from "jsonwebtoken";
+
 import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequestError } from "../errors/bad-request-error";
 
 const router = express.Router();
@@ -28,6 +30,13 @@ async (req: Request, res: Response) => {
 
   const user = User.build({ email, password });
   await user.save();
+
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, 'Asd.123');
+
+  req.session = { jwt: userJwt };
 
   res.status(201).send(user);
 });
