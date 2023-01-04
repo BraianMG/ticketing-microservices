a4848c11a1772bc1fec43e3031539b08af3b2223
Braian Gonzales <braian.gonzales@wakapi.com>
2023-01-04 14:34:14 -0300

    feat(auth): add session support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,15 @@ import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signt";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
 
 app.use(currentUserRouter);
 app.use(signinRouter);
