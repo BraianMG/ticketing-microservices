dc9ea3e95dd92340e6ce340c979b480e7154c041
Braian <braian.gonzales77@gmail.com>
2023-01-05 11:58:56 -0300

    feat(auth): add test for cookie

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -60,3 +60,15 @@ it('disallows diplicate emails', async () => {
     })
     .expect(400);
 });
+
+it('sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '123456'
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
